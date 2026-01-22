47b436a4b5eb1d0d7ebaff0b948b903aa136f98e
Alan Adamson <allanmg.77.90@gmail.com>
2026-01-22 10:47:08 +0100

    Fix index.js: remove duplicate redirection and fix ReactDOM import

--- cartamoki/src/index.js
+++ cartamoki/src/index.js
@@ -1,16 +1,7 @@
-//import ReactDOM from 'react-dom';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-
-// En src/index.js, antes de ReactDOM.render
-if (window.location.href !== 'https://developements.blankcodestudios.com/') {
-  window.location.href = 'https://developements.blankcodestudios.com/';
-} else {
-  // Renderizar la app normalmente
-  ReactDOM.render(<App />, document.getElementById('root'));
-}
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
