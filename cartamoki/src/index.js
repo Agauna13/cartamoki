52fbcebe739b96fb0b8790a6eec51e44b9976619
Alan Adamson <allanmg.77.90@gmail.com>
2026-01-22 10:36:08 +0100

    ...

--- cartamoki/src/index.js
+++ cartamoki/src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
 //import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import {createRoot} from 'react-dom/client';
 
 
+// En src/index.js, antes de ReactDOM.render
+if (window.location.href !== 'https://developements.blankcodestudios.com/') {
+  window.location.href = 'https://developements.blankcodestudios.com/';
+} else {
+  // Renderizar la app normalmente
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
